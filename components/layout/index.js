a7b3e4b4f529f0a3f507160d2e8ad9d39aa4b801
Apriadi <dev.apriadi@gmail.com>
2021-07-16 05:50:07 +0700

    make responsive layout

--- components/layout/index.js
+++ components/layout/index.js
@@ -8,9 +8,13 @@ const Layout = ({children, props}) => {
                 <title>{props.title}</title>
                 <meta name="description" content="Portfolio Apriadi, S.Si" />
             </Head>
-            <div className="flex">
-                <NavigasiMenu/>
-                { children }
+            <div className="flex-none md:flex">
+                <div className="w-full md:w-80" style={{ backgroundColor: '#54B689'}}>
+                    <NavigasiMenu/>
+                </div>
+                <div className="w-full md:w-auto">
+                    { children }
+                </div>
             </div>
         </div>
     )
