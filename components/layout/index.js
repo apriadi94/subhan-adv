67f1d127004b6f12e0e6447d06179364486af0c9
Apriadi <dev.apriadi@gmail.com>
2021-07-17 03:40:42 +0700

    refactory code

--- components/layout/index.js
+++ components/layout/index.js
@@ -4,20 +4,22 @@ import { GlobalProvider } from '../provider/globalProvider'
 
 const Layout = ({children, props}) => {
     return(
-        <GlobalProvider>
-            <Head>
-                <title>{props.title}</title>
-                <meta name="description" content="Portfolio Apriadi, S.Si" />
-            </Head>
-            <div className="flex-none md:flex">
-                <div className="w-full md:w-72 max-h-screen overflow-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch" style={{ backgroundColor: '#54B689'}}>
-                    <NavigasiMenu/>
+        <div>
+            <GlobalProvider>
+                <Head>
+                    <title>{props.title}</title>
+                    <meta name="description" content="Portfolio Apriadi, S.Si" />
+                </Head>
+                <div className="flex-none md:flex">
+                    <div className="w-full md:w-72 max-h-screen overflow-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch" style={{ backgroundColor: '#54B689'}}>
+                        <NavigasiMenu/>
+                    </div>
+                    <div className="w-full md:w-auto">
+                        { children }
+                    </div>
                 </div>
-                <div className="w-full md:w-auto">
-                    { children }
-                </div>
-            </div>
-        </GlobalProvider>
+            </GlobalProvider>
+        </div>
     )
 }
 
--- components/layout/navigation.js
+++ components/layout/navigation.js
@@ -1,16 +1,31 @@
+import { useRouter } from 'next/router'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook, faGithub, faInstagram, faLinkedinIn, faPinterest } from '@fortawesome/free-brands-svg-icons'
 import { faFileCode, faUser } from '@fortawesome/free-regular-svg-icons'
 import { faBlog, faLaptopCode, faNetworkWired } from '@fortawesome/free-solid-svg-icons'
 
+const NavMenu = ({ router, asPath, routePath, routeName, icon }) => {
+    return(
+        <div className="flex ml-4 cursor-pointer mt-5" onClick={() => router.push(routePath)}>
+            <div className="w-4">
+                <FontAwesomeIcon icon={icon} color={asPath === routePath ? '#275941' : '#DDF0E7'} size="sm" className="my-auto"/>
+            </div>
+            <h1 className="text-center font-bold text-math ml-5 my-auto mt-1" style={{ color: asPath === routePath ? '#275941' : '#DDF0E7' }}>{routeName}</h1>
+    </div>
+    )
+}
+
 const NavigasiMenu = () => {
+    const router = useRouter()
+    const { asPath } = useRouter()
+
     return(
         <div className="pt-5 mx-5">
            <div>
                <h1 className="text-center font-bold text-4xl text-white text-math">Apriadi</h1>
                <img className="mt-5 w-40 h-40 rounded-full mx-auto" src="/apriadi.jpg"/>
                <p className="text-center text-sm mt-2 text-white text-math">
-                Hi, my name is Apriadi and I'm a junior javascript developer. welcome to my portfolio website!
+                    Hi, my name is Apriadi and I'm a junior javascript developer. welcome to my portfolio website!
                </p>
                <div className="mx-auto ml-28 mr-28 p-4 mt-4 md:ml-10 md:mr-10 grid grid-cols-4">
                     <div className="bg-white w-7 h-7 rounded-full text-center">
@@ -31,43 +46,14 @@ const NavigasiMenu = () => {
                     <div className="mx-5 mb-5">
                         <hr className="w-full" style={{ borderColor: '#4DA87E' }}/>
                     </div>
+                    <NavMenu router={router} asPath={asPath} routePath={'/'} routeName={'About Me'} icon={faUser}/>
+                    <NavMenu router={router} asPath={asPath} routePath={'/project'} routeName={'Project'} icon={faLaptopCode}/>
+                    <NavMenu router={router} asPath={asPath} routePath={'/resume'} routeName={'Resume / CV'} icon={faFileCode}/>
+                    <NavMenu router={router} asPath={asPath} routePath={'/service'} routeName={'Service & Pricing'} icon={faNetworkWired}/>
+                    <NavMenu router={router} asPath={asPath} routePath={'/github'} routeName={'Github Activity'} icon={faGithub}/>
+                    <NavMenu router={router} asPath={asPath} routePath={'/contact'} routeName={'Contact'} icon={faPinterest}/>
+                    <NavMenu router={router} asPath={asPath} routePath={'/blog'} routeName={'My Blog'} icon={faBlog}/>
 
-                    <div className="flex ml-4">
-                        <div className="w-4">
-                            <FontAwesomeIcon icon={faUser} color="#DDF0E7" size="sm" className="my-auto"/>
-                        </div>
-                        <h1 className="text-center font-bold text-math ml-5 my-auto mt-1" style={{ color: '#DDF0E7' }}>About Me</h1>
-                    </div>
-                    <div className="flex ml-4 mt-5">
-                        <div className="w-4">
-                            <FontAwesomeIcon icon={faLaptopCode} color="#DDF0E7" size="sm" className="my-auto"/>
-                        </div>                        
-                        <h1 className="text-center font-bold text-math ml-5 my-auto mt-1" style={{ color: '#DDF0E7' }}>My Project</h1>
-                    </div>
-                    <div className="flex ml-4 mt-5">
-                        <div className="w-4">
-                            <FontAwesomeIcon icon={faFileCode} color="#DDF0E7" size="sm" className="my-auto"/>
-                        </div>                           
-                        <h1 className="text-center font-bold text-math ml-5 my-auto mt-1" style={{ color: '#DDF0E7' }}>Resume / CV</h1>
-                    </div>
-                    <div className="flex ml-4 mt-5">
-                        <div className="w-4">
-                            <FontAwesomeIcon icon={faNetworkWired} color="#DDF0E7" size="sm" className="my-auto"/>
-                        </div>                           
-                        <h1 className="text-center font-bold text-math ml-5 my-auto mt-1" style={{ color: '#DDF0E7' }}>Service & Pricing</h1>
-                    </div>
-                    <div className="flex ml-4 mt-5">
-                        <FontAwesomeIcon icon={faGithub} color="#DDF0E7" size="sm" className="my-auto"/>
-                        <h1 className="text-center font-bold text-math ml-5 my-auto mt-1" style={{ color: '#DDF0E7' }}>Github Activity</h1>
-                    </div>
-                    <div className="flex ml-4 mt-5">
-                        <FontAwesomeIcon icon={faPinterest} color="#DDF0E7" size="sm" className="my-auto"/>
-                        <h1 className="text-center font-bold text-math ml-5 my-auto mt-1" style={{ color: '#DDF0E7' }}>Contact</h1>
-                    </div>
-                    <div className="flex ml-4 mt-5">
-                        <FontAwesomeIcon icon={faBlog} color="#DDF0E7" size="sm" className="my-auto"/>
-                        <h1 className="text-center font-bold text-math ml-5 my-auto mt-1" style={{ color: '#DDF0E7' }}>My Blog</h1>
-                    </div>
               </div>
 
               <div className="flex justify-center mt-10 mb-5">
